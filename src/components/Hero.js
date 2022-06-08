5b11487a2af3b6f0b975177886c97d97b1d5e22a
Dujin <kdj00718@gmail.com>
2022-06-08 13:49:08 -0700

    initial

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <img src={"./img/hero.png"} alt="hero" className="hero--photo"/>
+      <h1>Online Experience</h1>
+      <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+    </div>
+  )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Nav() {
+  return (
+      <nav>
+        <img src={"../img/logo.png"} alt="logo" />
+      </nav>
+  )
+}
